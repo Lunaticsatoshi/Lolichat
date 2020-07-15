f563b6b7118b6231d8a02371ea7fd13abbc3e95f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-07-15 21:32:07 +0530

    Made the chat server

--- Lolichat/website/static/client.js
+++ Lolichat/website/static/client.js
@@ -4,6 +4,9 @@ window.addEventListener('DOMContentLoaded', () => {
     ws = new WebSocket(`ws://localhost:3000/ws`);
     ws.addEventListener('open', onConnectionOpen);
     ws.addEventListener('open', onMessageRecieved);
+
+    const queryParams = getQueryParams()
+    console.log(queryParams);
 })
 
 function onConnectionOpen(){
@@ -12,4 +15,16 @@ function onConnectionOpen(){
 
 function onMessageRecieved(event){
     console.log('Recieved message', event);
+}
+
+function getQueryParams(){
+    const search = window.location.search.substring(1);
+    const parameters = search.split('&');
+    const params = {};
+    for (const parameter of parameters){
+        const parts = parameter.split('=');
+        params[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+    }
+
+    return params;
 }
