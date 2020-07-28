8463c97f60beb1de9bb533ae9d504f483c2ec8ae
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-07-28 21:33:53 +0530

    Changed some styles and added user display functionality

--- Lolichat/chat.js
+++ Lolichat/chat.js
@@ -1,3 +1,62 @@
+import { isWebSocketCloseEvent } from "https://deno.land/std@0.58.0/ws/mod.ts";
+import { v4 } from "https://deno.land/std@0.58.0/uuid/mod.ts";
+
+const userMap = new Map();
+
+const groupsMap = new Map();
+
 export default async function chat(ws){
     console.log('Connected');
+    const userId = v4.generate();
+
+    for await (let data of ws){
+        console.log(data, typeof data);
+        const event = typeof data === 'string' ? JSON.parse(data) : data;
+        if (isWebSocketCloseEvent(data)){
+            const user = userMap.get(userId);
+            let users = groupsMap.get(user.groupName) || [];
+            users = users.filter(u => u.userId !== userId);
+            groupsMap.set(user.groupName, users);
+            userMap.delete(userId);
+            
+            emitEvent(user.groupName);
+            break;
+        }
+        switch (event.event){
+            case 'join':
+                const user = {
+                    userId,
+                    name: event.name,
+                    groupName: event.groupName,
+                    ws
+                };
+                userMap.set(userId, user);
+                const users = groupsMap.get(event.groupName) || [];
+                users.push(user);
+                groupsMap.set(event.groupName, users);
+
+                emitEvent(event.groupName)
+        }
+    }
+}
+
+function emitEvent(groupName) {
+    const users = groupsMap.get(groupName);
+    for (const user of users){
+        const event = {
+            event: 'users',
+            data: getDisplayUsers(groupName)
+        }
+        user.ws.send(JSON.stringify(event))
+    }
+}
+
+function getDisplayUsers(groupName) {
+    const users = groupsMap.get(groupName) || [];
+    return users.map((u) => {
+        return {
+            userId: u.userId,
+            name: u.name,
+        }
+    })
 }
--- Lolichat/website/static/client.js
+++ Lolichat/website/static/client.js
@@ -1,20 +1,44 @@
 let ws;
+let chatUsersCont = document.querySelector('#chatUsers');
+let chatusersCount = document.querySelector('#chatUsersCount');
 
 window.addEventListener('DOMContentLoaded', () => {
     ws = new WebSocket(`ws://localhost:3000/ws`);
     ws.addEventListener('open', onConnectionOpen);
-    ws.addEventListener('open', onMessageRecieved);
-
-    const queryParams = getQueryParams()
-    console.log(queryParams);
+    ws.addEventListener('message', onMessageRecieved);
 })
 
 function onConnectionOpen(){
     console.log('Establishing Connection');
+    const queryParams = getQueryParams()
+    console.log(queryParams);
+    if (!queryParams.name || !queryParams.group){
+        window.location.href = 'chat.html';
+        return
+    }
+    const event = {
+        event: 'join',
+        groupName: queryParams.group,
+        name: queryParams.name
+    }
+    ws.send(JSON.stringify(event));
 }
 
 function onMessageRecieved(event){
     console.log('Recieved message', event);
+    event = JSON.parse(event.data);
+    console.log(event);
+    switch(event.event) {
+        case 'users':
+            chatUsersCont.innerHTML = '';
+            chatusersCount.innerHTML = event.data.length
+            event.data.forEach(u => {
+                const user1 = document.createElement('div')
+                user1.className = 'chat-user';
+                user1.innerHTML = u.name;
+                chatUsersCont.appendChild(user1)
+            })
+    }
 }
 
 function getQueryParams(){
